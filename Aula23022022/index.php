3e7a79d9cbfd05f746f741f3fb0813a8559b9083
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-02-23 20:37:13 -0300

    Atualização

--- Aula23022022/index.php
+++ Aula23022022/index.php
@@ -21,7 +21,10 @@
             $umaVariavel = 20;
             $umaVariavel = "Vanessa";
             echo "Meu nome é $umaVariavel"; //se usar aspas duplas, será exibido o conteudo da variável; aspas simples, exibido como texto
-
+            $total = 30 * 10 / 2 - 4+ 1;
+            $total++;
+            $total--;
+            --$total; //a diferença entre decrementar/incrementar antes e depois é no processamento (primeiro incrementa/decrementa, depois processa)
         ?>  
     </h1> <!--Y maiusculo para exibir o ano com 4 dígitos !-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
