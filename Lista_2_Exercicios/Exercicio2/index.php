fe17c4264c7d18dcd1a2ecfa3ee7e6d593337aaf
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-16 20:18:54 -0300

    Exercicio2 inicio

---
+++ Lista_2_Exercicios/Exercicio2/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Exercicio 2</title>
+  </head>
+  <body class="container mt-5">
+    <h1>Exercicio 2</h1>
+    <form action="resultado.php" method="post">
+      <div class="row">
+        <?php
+          for($i=1; $i<=10; $i++){
+        ?>
+          <div class="col">
+            <label for="valor<?= $i ?>" class="label-control">
+              Informe o valor <?= $i ?>
+            </label>
+            <input type="number" name="valor<?= $i ?>" id="valor<?= $i ?>" class="form-control"/>
+          </div>
+        <?php
+          }
+        ?>
+      </div>
+      <div class="row">
+        <?php
+          for($i=11; $i<=20; $i++){
+        ?>
+          <div class="col">
+            <label for="valor<?= $i ?>" class="label-control">
+              Informe o valor <?= $i ?>
+            </label>
+            <input type="number" name="valor<?= $i ?>" id="valor<?= $i ?>" class="form-control"/>
+          </div>
+        <?php
+          }
+        ?>
+      </div>
+      <div class="row">
+        <div class="col"> 
+          <button class="btn btn-primary mt-2">
+            Enviar
+          </button>
+        </div>
+      </div>
+    </form>
+
+   
+  </body>
+</html>
