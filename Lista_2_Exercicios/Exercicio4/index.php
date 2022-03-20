05f6cb8328ac095a6f492ede5c9a2c83b6e76365
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-20 14:51:29 -0300

    atualização com exercicio 4

---
+++ Lista_2_Exercicios/Exercicio4/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Exercicio 4</title>
+  </head>
+  <body class="container mt-5">
+    <h1>Média dos elementos - Mapa Ordenado</h1>
+    <form action="resultado.php" method="post">
+      <div class="row">
+        <?php
+          for($i=1; $i<=10; $i++){
+        ?>
+          <div class="col">
+            <label for="valor<?= $i ?>" class="label-control">
+              Informe a nota do aluno <?= $i ?>
+            </label>
+            <input type="text" name="valor<?= $i ?>" id="valor<?= $i ?>" class="form-control"/>
+          </div>
+        <?php
+          }
+        ?>
+      </div>
+      <div class="row">
+        <div class="col"> 
+          <button class="btn btn-primary mt-2">
+            Enviar
+          </button>
+        </div>
+      </div>
+    </form>
+  </body>
+</html>
--- Lista_2_Exercicios/Exercicio4/resultado.php
+++ Lista_2_Exercicios/Exercicio4/resultado.php
@@ -6,10 +6,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Resposta Exercício 3</title>
+    <title>Resposta Exercício 4</title>
   </head>
   <body class="container">
-    <h1>Resposta Exercicio 3</h1>
+    <h1>Resposta Exercicio 4</h1>
 
     <?php
 
@@ -17,28 +17,14 @@
             $vetor[$i]= $_POST["valor$i"];
         }
 
-        echo  "Antes da substituições";
+        $soma = 0;
         for ($i=1; $i<=10; $i++){
-            echo "<br>";
-            echo "O item de posição $i tem valor $vetor[$i]";
-        }
-        echo "<br>";
-        echo "Após as substituções";
-        $hifen = "-";
-        // var_dump($vetor);
-        for ($i=1; $i<=10; $i++){
-            for ($j=1; $j<=10; $j++){
-                if ($vetor[$i] == $vetor[$j] && $vetor[$i] != $hifen && $i != $j){
-                    $vetor[$j] = $hifen;
-                }
-            }
+            $soma = $soma + $vetor[$i];
         }
 
-        for ($i=1; $i<=10; $i++){
-            echo "<br>";
-            echo "O item de posição $i tem valor $vetor[$i]";
-        }
-        
+        $media = $soma / 10;
+        echo "A média dos alunos é $media";
+             
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
