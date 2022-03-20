41f38491f8a5295badbd589fef6c2b81cb3dd1d9
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-20 12:37:42 -0300

    Atualização Exercicio 2

--- Lista_2_Exercicios/Exercicio2/resultado.php
+++ Lista_2_Exercicios/Exercicio2/resultado.php
@@ -17,19 +17,43 @@
         $vetor[$i] = $_POST["valor$i"];
       }
 
+      $trava = 0;
       $qtde = 0;
+      //procurando valor inválido
       foreach ($vetor as $chave => $valor){
         if ($valor<1 || $valor>10){
-          echo "Existem valores inválidos nos dados";
-          break;
+          echo "Existe valor inválido na posição $chave.";
+          $trava = $trava + 1;
         }
-        else ($valor == $valorproc){
-          $qtde = $qtde + 1;
-          echo "Posição do valor procurado: $chave";
-          echo "<br>";
-        }        
       }
-      echo "O valor $valorproc está em $qtde posições.";
+
+      if ($trava == 0){
+        foreach ($vetor as $chave => $valor){
+          if ($valor == $valorproc){
+            $qtde = $qtde + 1;
+            echo "O valor $valorproc está na posição $chave";
+            echo "<br>";
+          }
+        }
+
+        if ($qtde == 0){
+          if ($valorproc>=1 && $valorproc<=10){
+            echo "<br>";
+            echo "Não há o valor $valorproc entre os 20 valores";
+          }
+          else{
+            echo "Este valor procurado está fora do intervalo solicitado para digitação.";
+          }
+          
+        }
+        else{
+          echo "<br>";
+          echo "O valor $valorproc está em $qtde posições.";
+        }
+      }
+
+      
+      
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
