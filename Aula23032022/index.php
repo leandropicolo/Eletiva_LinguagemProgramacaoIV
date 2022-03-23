b95059a3c9d4e09e307c940c518e797ae370e782
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-22 23:31:59 -0300

    Create index.php

---
+++ Aula23032022/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Funções</title>
+  </head>
+  <body>
+    <h1>Funções</h1>
+	
+	<?= date("y/m/Y")?>
+	
+	<br/>
+	
+	<?= date("h:i") ?>
+
+  <br/>
+
+  <?php
+    $valor = 10004.789;
+    echo "R$".(number_format($valor, 2, ',', '.'));
+  ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
