3a10d66ce7b6d29d095a541cf3833f5afe9653be
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-22 23:59:42 -0300

    inicio exercicio 1 lista 3

--- Lista_3_Exercicios/resposta.php
+++ Lista_3_Exercicios/resposta.php
@@ -6,36 +6,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Funções</title>
+    <title>Resposta - Exercicio 1</title>
   </head>
-  <body class="container">
-    <h1>Funções</h1>
-	
-	<?= date("y/m/Y")?>
-	
-	<br/>
-	
-	<?= date("h:i") ?>
-
-  <br/>
-
-  <?php
-    function formatarNumero($numero){
-      echo "R$".(number_format($numero, 2, ',', '.'));
-    }
-
-    function retornarNumero($numero){
-      return "R$".(number_format($numero, 2, ',', '.'));
-    }
-
-    formatarNumero(10004.789);
-    echo "<br/>";
-    echo retornarNumero(897.111);
-    $numero = retornarNumero(150.669);
-    echo "<br/>";
-    echo $numero;
-
-  ?>
+  <body>
+    <h1>Resposta - Exercicio 1</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
