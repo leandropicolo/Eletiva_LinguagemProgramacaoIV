807029f7effa839dd9a33fd84197ad4d3a7ad309
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-25 21:00:31 -0300

    exercicio 4

--- Lista_3_Exercicios/Exercicio4/resultado.php
+++ Lista_3_Exercicios/Exercicio4/resultado.php
@@ -6,25 +6,40 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Resposta - Exercicio 1</title>
+    <title>Resposta - Exercicio 4</title>
   </head>
   <body class="container">
-    <h1>Resposta - Exercicio 1</h1>
+    <h1>Resposta - Exercicio 4</h1>
 
     <?php
-        function verificarData($dia, $mes, $ano){
-            if (checkdate($mes, $dia, $ano))
-                echo "Essa é a data válida!!";
-            else{
-                echo "Essa não é uma data válida!!";
+        
+        $numero1 = $_POST['numero1'];
+        $numero2 = $_POST['numero2'];
+        $numero3 = $_POST['numero3'];
+        $numero4 = $_POST['numero4'];
+        $numero5 = $_POST['numero5'];
+
+        function somaDivisores($valor){
+            $soma = 0;
+            for ($i = 1; $i <= $valor; $i++){
+              if ($valor % $i == 0 && $valor != $i){
+                 $soma = $soma + $i;
+              }
             }
+            echo "O resultado da soma dos divisores de $valor é ".$soma;
+            echo "<br>";
         }
 
-        $diaInformado = $_POST['dia'];
-        $mesInformado = $_POST['mes'];
-        $anoInformado = $_POST['ano'];
-
-        verificarData($diaInformado, $mesInformado, $anoInformado);
+        if ($numero1 <= 0 || $numero2 <= 0 || $numero3 <= 0 || $numero4 <= 0 || $numero5 <= 0 ){
+          echo "Um dos números informados é menor ou igual a zero. Reinicie o programa!!!";
+        }
+        else{
+          somaDivisores($numero1);
+          somaDivisores($numero2);
+          somaDivisores($numero3);
+          somaDivisores($numero4);
+          somaDivisores($numero5);
+        }
 
     ?>
 
