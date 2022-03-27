a6bb2eb7c6df7dea3f22bf52c40781c5c44761ed
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-03-27 18:51:01 -0300

    lista 3 concluida

--- Lista_3_Exercicios/Exercicio5/resultado.php
+++ Lista_3_Exercicios/Exercicio5/resultado.php
@@ -6,25 +6,46 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Resposta - Exercicio 1</title>
+    <title>Resposta - Exercicio 5</title>
   </head>
   <body class="container">
-    <h1>Resposta - Exercicio 1</h1>
+    <h1>Resposta - Exercicio 5</h1>
 
     <?php
-        function verificarData($dia, $mes, $ano){
-            if (checkdate($mes, $dia, $ano))
-                echo "Essa é a data válida!!";
-            else{
-                echo "Essa não é uma data válida!!";
+        function calcularIMC($altura, $peso){
+            $imc = $peso / ($altura * $altura);
+            echo "O IMC desta pessoa é ".$imc;
+            echo "<br>";
+            if ($imc <= 18.5){
+                echo "Abaixo do peso";
+                echo "<br>";
+            }
+            elseif ($imc > 18.5 && $imc <= 24.9) {
+                echo "Peso ideal (parabéns)";
+                echo "<br>";
+            }
+            elseif ($imc > 25 && $imc <= 29.9) {
+                echo "Levemente acima do peso";
+                echo "<br>";
+            }
+            elseif ($imc > 30 && $imc <= 34.9) {
+                echo "Obesidade grau I";
+                echo "<br>";
+            }
+            elseif ($imc > 35 && $imc <= 39.9) {
+                echo "Obesidade grau II (severa)";
+                echo "<br>";
+            }
+            else {
+                echo "Obesidade grau III (mórbida)";
+                echo "<br>";
             }
         }
 
-        $diaInformado = $_POST['dia'];
-        $mesInformado = $_POST['mes'];
-        $anoInformado = $_POST['ano'];
+        $alturaInformada = $_POST['altura'];
+        $pesoInformado = $_POST['peso'];
 
-        verificarData($diaInformado, $mesInformado, $anoInformado);
+        calcularIMC($alturaInformada, $pesoInformado);
 
     ?>
 
