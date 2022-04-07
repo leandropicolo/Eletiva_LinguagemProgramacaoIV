ce59d7d1d88ee20679f071b8948142be239ee4e3
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-04-06 21:32:27 -0300

    update ex 1 lista 4

--- Lista_4_Exercicios/Exercicio_1/index.php
+++ Lista_4_Exercicios/Exercicio_1/index.php
@@ -11,6 +11,33 @@
   <body class="container">
     <h1>Dados da Corrida - Exercicio 1</h1>
 
+    <div class="row">
+        <div class="col">
+            <div class="modal mt-5" tabindex="-1" style="display: block">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-body">
+                        <form action="voltas.php" method="POST">
+                                <div class="col">
+                                    <label for="voltas" classe="label-control">
+                                        Digite a quantidade de voltas:
+                                    </label>
+                                    <input type="text" name="voltas" id="voltas" class="form-control">
+                                </div>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Digitar tempos</button>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
    
