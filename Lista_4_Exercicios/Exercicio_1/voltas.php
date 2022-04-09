b6dee87f07da1433152efc0d758867a1adb8c067
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-04-08 21:50:26 -0300

    update ex 1 lista 4

--- Lista_4_Exercicios/Exercicio_1/voltas.php
+++ Lista_4_Exercicios/Exercicio_1/voltas.php
@@ -16,26 +16,38 @@
     ?>
         <div class="row">
         <?php
-          for($i=1; $i<=$voltas; $i++){
-        ?>
-          <div class="col">
-            <label for="voltas<?= $i ?>" class="label-control">
-              Informe a <?= $i ?>ª volta:
-            </label>
+          session_start();
+          if ($voltas > 0){
+            for($i=1; $i<=$voltas; $i++){
+          ?>
+            <div class="col">
+              <label for="voltas<?= $i ?>" class="label-control">
+                Informe a <?= $i ?>ª volta:
+              </label>
             <input type="text" name="voltas<?= $i ?>" id="voltas<?= $i ?>" class="form-control"/>
+            </div>
+          
           </div>
-        <?php
+          <?php
+            }
+
+          ?>
+          <div class="row">
+            <div class="col"> 
+              <button type="submit" class="btn btn-primary mt-2">
+                Calcular
+              </button>
+            </div>
+          </div>
+          
+          <?php
+          } else {
+            header('Location: index.php');
+            exit;
           }
-        ?>
-      </div>
-      <div class="row">
-        <div class="col"> 
-          <button type="submit" class="btn btn-primary mt-2">
-            Calcular
-          </button>
-        </div>
-      </div>
-    
+
+          ?>
+          
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
