7826d75ffa3b27d61a3bde860f24940b609010d8
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-04-25 00:35:55 -0300

    Resolução ex 1 Session

--- Lista_4_Exercicios/Exercicio_1/calculos.php
+++ Lista_4_Exercicios/Exercicio_1/calculos.php
@@ -13,12 +13,13 @@
 
     <?php
       $soma = 0;
+      session_start();
       $voltas = $_SESSION['voltas'];
       for($i=1; $i<=$voltas; $i++){
         $soma = $soma + $_POST["voltas$i"];
       }
       $media = $soma / $voltas;
-      echo "A média é" + $media;
+      echo "O resultado é $media.";
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
