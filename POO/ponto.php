b69334adf310352b61a8c76c7eb157153b7edea1
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-04-29 22:05:47 -0300

    update

--- POO/ponto.php
+++ POO/ponto.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-class Ponto{
+class Ponto
+{
     private $x;
     private $y;
     private static $contador;
@@ -9,19 +10,37 @@ class Ponto{
     {
         $this->setx($x);
         $this->sety($y);
+        self::setContador();
     }
 
-    public function calcularDistancia(Ponto $p){
-        $valorx = pow(($p->getx- $this->x), 2);
-        $valory = pow(($p->gety- $this->y), 2);
+    public function calcularDistancia(Ponto $p)
+    {
+        $valorx = pow(($p->getx - $this->x), 2);
+        $valory = pow(($p->gety - $this->y), 2);
         return sqrt($valorx + $valory);
     }
 
-    private static function setContador(){
+    public function calcularDistancia2($x2, $y2)
+    {
+        $valorx = pow(($x2 - $this->x), 2);
+        $valory = pow(($y2 - $this->y), 2);
+        return sqrt($valorx + $valory);
+    }
+
+    public function calcularDistancia3($x1, $x2, $y1, $y2)
+    {
+        $valorx = pow(($x2 - $x1), 2);
+        $valory = pow(($y2 - $y1), 2);
+        return sqrt($valorx + $valory);
+    }
+
+    private static function setContador()
+    {
         self::$contador++;
     }
 
-    public static function getContador(){
+    public static function getContador()
+    {
         return self::$contador;
     }
 
@@ -44,5 +63,6 @@ class Ponto{
     {
         $this->y = $y;
     }
-
 }
+
+?>
