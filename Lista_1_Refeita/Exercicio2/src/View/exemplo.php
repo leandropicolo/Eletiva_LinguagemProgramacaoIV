1393ff271487127f5676ae1e5fbe57e29b7985a1
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-05-22 22:23:42 -0300

    Update exemplo.php

--- Lista_1_Refeita/Exercicio2/src/View/exemplo.php
+++ Lista_1_Refeita/Exercicio2/src/View/exemplo.php
@@ -31,7 +31,7 @@
 
     <?php
         if (isset($resultado))
-            echo "O resultado é $resultado";
+            echo "$resultado";
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
